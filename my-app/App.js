2c9884e8ebd0c81352d639223643a80644ad43da
moe booka <mhmdbkaili@hotmail.com>
2024-05-21 23:52:21 -0400

    Updated App.js
    Text input and button functionality - pressing button after inputting text adds the text to a styled list of goals

--- my-app/App.js
+++ my-app/App.js
@@ -1,16 +1,41 @@
 // import { StatusBar } from 'expo-status-bar';
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  // useState to save input
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  // useState to save the list of added goals
+  const [courseGoals, setCourseGoals] = useState([]);
+  // set entered text
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+  // add goal button functionality
+  function addGoalHandler() {
+    // arrow function that creates a new list made of the old list destructured and a new element appended at tail
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  }
   return (
     // view is equivalent to HTML div - View holds components but is not able to render text
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your Course Goal" />
-        <Button title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Course Goal"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List Of Goals</Text>
+        {courseGoals.map((goal, key) => (
+          <View key={key} style={styles.goalItem}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -42,5 +67,12 @@ const styles = StyleSheet.create({
   },
   goalsContainer: {
     flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 8,
+    backgroundColor: "#000fff",
+    color: "white",
   },
 });
